e0b2ee82bba47ec11fa6db98b23d34e9d848173f
riz <mr.ridwan.thekeeper@gmail.com>
2024-06-03 14:55:40 +0700

    03 - image and font optimization

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Lusitana, Poppins} from "next/font/google"
+
+export const poppins = Poppins({weight: "400", subsets: ["devanagari"]})
+export const lusitana = Lusitana({weight: ["400", "700"], subsets: ["latin"]})
